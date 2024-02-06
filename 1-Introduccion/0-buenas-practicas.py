b4a92fa969cd379f84079d0fda074c99b365ed5e
Mariano Garcia <marianogarciagz@gmail.com>
2024-02-05 21:17:45 -0600

    first commit

---
+++ 1-Introduccion/0-buenas-practicas.py
@@ -0,0 +1,12 @@
+# Buenas practicas
+
+# snake_case para variables
+
+# UPPERCASE para constantes
+
+# inputs
+
+name = input("Ingresa tu nombre: ")
+
+print("Tu nombre es: " + name)
+
---
+++ 1-Introduccion/1-tipo-de-datos.py
@@ -0,0 +1,23 @@
+# tipos de datos
+
+# String
+
+name="Mariano"
+
+# Int
+
+age= 21
+
+# Floar
+
+score = 99.9
+
+# Boolean
+
+Status = True
+
+# print(name,age,score,active)
+
+print("Nombre: " + name)
+print("Edad: " + age)
+print("¿Está activo?: " + score)
