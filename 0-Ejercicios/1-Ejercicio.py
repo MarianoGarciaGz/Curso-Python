a6115b30ea3668a1e4ef1a692f9a104138c9af19
Mariano Garcia <marianogarciagz@gmail.com>
2024-02-17 12:54:02 -0600

    :sparkles: Feat: add 1° example

--- 0-Ejercicios/1-Ejercicio.py
+++ 0-Ejercicios/1-Ejercicio.py
@@ -1,3 +1,18 @@
 print("Si tu nombre aparece en la lista de invitados, puedes pasar")
-name: input("Introduce tu nombre: ")
+name= str(input("Introduce tu nombre: "))
 
+list_names = ["Mariano", "Oscar", "Alberto", "Andrea", "Abigail", "Guillermo", "Fernanda", "Fernando", "María", "Francisco"]
+name_match = False
+
+for i in range(0, len(list_names)):
+  if str(list_names[i]) == name:
+    name_match= True
+    break
+  else:
+    name_match= False
+    continue
+
+if name_match == True:
+  print("Estás en la lista de invitados")
+else:
+  print("No estás en la lista de invitados")
